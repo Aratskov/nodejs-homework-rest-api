86fce1e66fdf953b3975023017bb8d8a23fbb334
Andrii <ifromukraine2022@gmail.com>
2023-05-05 15:30:53 +0300

    init

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-const app = require('./app')
+// mongodb+srv://ifromukraine2022:<password>@cluster0.exi0wfz.mongodb.net/?retryWrites=true&w=majority
+const mongoose = require("mongoose");
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://ifromukraine2022:sl9udfuzDD6K1SeM@cluster0.exi0wfz.mongodb.net/contacts_db?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
