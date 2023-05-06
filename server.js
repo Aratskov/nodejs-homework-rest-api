3e127cc49ffe5ea50cc2508e2740fc2bb5fb00b4
Andrii <ifromukraine2022@gmail.com>
2023-05-06 13:13:08 +0300

    add env

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+// mongodb+srv://ifromukraine2022:<password>@cluster0.exi0wfz.mongodb.net/?retryWrites=true&w=majority
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const { DB_HOST } = process.env;
+// const DB_HOST =
+  "mongodb+srv://ifromukraine2022:sl9udfuzDD6K1SeM@cluster0.exi0wfz.mongodb.net/contacts_db?retryWrites=true&w=majority";
+
+  const {DB_HOST, PORT = 3000} = process.env
 
 mongoose.set("strictQuery", true);
 
@@ -9,7 +13,7 @@ mongoose
   .connect(DB_HOST)
   .then(() => {
     console.log("Database connection successful");
-    app.listen(3000);
+    app.listen(PORT);
   })
   .catch((error) => {
     console.log(error.message);
