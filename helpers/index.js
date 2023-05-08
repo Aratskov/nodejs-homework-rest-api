9a98486ffa059defe10b45730bdc9c973bbc9f3f
Andrii <ifromukraine2022@gmail.com>
2023-05-08 15:04:41 +0300

    add task 3

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,9 @@
 const HttpError = require("./HttpError");
 const ctrlWrapper = require("./ctrlWrapper");
+const handleMoongoseError = require("./handlerMoongoseError")
 
 module.exports = {
   HttpError,
   ctrlWrapper,
+  handleMoongoseError
 };
