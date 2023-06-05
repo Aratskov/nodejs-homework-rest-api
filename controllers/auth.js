d53c48d3bd976ef66d80d13fcd3dafa8a430e025
Andrii <ifromukraine2022@gmail.com>
2023-06-05 23:51:32 +0300

    make delete avatar in folde temp

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,7 +7,7 @@ const path = require("path");
 const fs = require("fs/promises");
 const Jimp = require("jimp");
 
-const avavtarDir = path.join(__dirname, "../", "public", "avatars");
+const avatarDir = path.join(__dirname, "../", "public", "avatars");
 
 const { SECRET_KEY } = process.env;
 
@@ -73,13 +73,15 @@ const updateAvatar = async (req, res) => {
   const { _id } = req.user;
   const { path: tempUpload, originalname } = req.file;
   const filename = `${_id}_${originalname}`;
-  const resultUpload = path.join(avavtarDir, filename);
-  // await fs.rename(tempUpload, resultUpload);
+  const resultUpload = path.join(avatarDir, filename);
   const image = await Jimp.read(tempUpload)
   await image.resize(250,250)
   await image.writeAsync(resultUpload)
+  // await fs.rename(tempUpload, resultUpload);
   const avatarURL = path.join("avatars", filename);
   await User.findByIdAndUpdate(_id, { avatarURL });
+  
+  await fs.unlink(tempUpload)
 
   res.json({ avatarURL });
 };
